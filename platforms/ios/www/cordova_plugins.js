ec58d580746931fc58d579fd4ea9220e51870106
Marcelo Colla <marcelo@emid.com.br>
2015-05-12 10:49:54 -0300

    Add plugin ios BackgroundAudio

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -2,6 +2,8 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "nl.x-services.plugins.backgroundaudio": "1.0.1"
+}
 // BOTTOM OF METADATA
 });
